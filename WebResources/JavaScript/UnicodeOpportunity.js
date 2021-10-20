f137fef751d842da19a1dee5ec70d259ee5a7188
gabrielaraujog9 <gabrielaraujogoncalves14@gmail.com>
2021-10-20 17:45:21 -0300

    Gabriel - enviando codigo git

--- WebResources/JavaScript/UnicodeOpportunity.js
+++ WebResources/JavaScript/UnicodeOpportunity.js
@@ -2,6 +2,15 @@ if (typeof (Unicode) == "undefined") { Unicode = {} }
 if (typeof (Unicode.Opportunity) == "undefined") { Unicode.Account = {} }
 
 Unicode.Opportunity = {
+    Attributos: {
+        leg_oportunidadeid: "leg_oportunidadeid"
+    },
+    IdOnChange = function(context) {
+        var formContext = context.getFormContext();
 
+        var attributeName = Unicode.Account.Attributos.leg_oportunidadeid;
+
+       formContext.getAttribute(attributeName).setValue("laranja");
+    },
 
 }
