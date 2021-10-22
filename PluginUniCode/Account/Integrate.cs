3d0a863fcd31ff6246b7b3de752bd7e02b4ea5b6
gabrielaraujog9 <gabrielaraujogoncalves14@gmail.com>
2021-10-21 22:45:54 -0300

    gabriel comentario

--- PluginUniCode/Account/Integrate.cs
+++ PluginUniCode/Account/Integrate.cs
@@ -18,6 +18,7 @@ namespace PluginUniCode.Account
                 throw new InvalidPluginExecutionException("Conexão realizada com sucesso");
             else
                 throw new InvalidPluginExecutionException("Conexão não realiza");
+            //gabriel
         }
 
         public static IOrganizationService GetCrmService()
