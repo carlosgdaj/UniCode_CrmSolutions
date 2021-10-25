96274f23d1b98dad01498a447ae14df43986a55d
gabrielaraujog9 <gabrielaraujogoncalves14@gmail.com>
2021-10-24 23:10:16 -0300

    Gabriel - Notificação ao Cliente, usando message Win

--- WebResources/JavaScript/UnicodeOpportunity.js
+++ WebResources/JavaScript/UnicodeOpportunity.js
@@ -1,55 +1,12 @@
-if (typeof Unicode == 'undefined') {
-    Unicode = {};
-}
-if (typeof Unicode.Opportunity == 'undefined') {
-    Unicode.Opportunity = {};
-}
+if (typeof (Unicode) == 'undefined') { Unicode = {} }
+if (typeof (Unicode.Opportunity) == 'undefined') { Unicode.Opportunity = {} }
 
-Unicode.Opportunity = {
-    statecode={
-        Aberto: 0,
-        Ganho: 1,
-        Fechado: 2
-    },
+Unicode.Opportunity = {    
     Attributos: {
-        leg_oportunidadeid: 'leg_oportunidadeid',
-        statecode: "statecode",
-        parentcontactid: "parentcontactid"
-    },
-    OnSaveStatusCode: function (context) {
-        var formContext = context.getFormContext();
-        var attributeName = Unicode.Opportunity.Attributos.statecode;
-
-        var status = formContext.getAttribute(attributeName).getValue();
-
-        if (status == Unicode.Opportunity.statecode.Ganho) {
-
-            var contact = formContext.getAttribute(Unicode.Opportunity.Attributos.parentcontactid).getValue();
-            CreateNotification(contact[0].id, contact[0].name);
-
-
-        }
-    },
-    CreateNotification: function (contactId, contactName) {
-        
-        var entity = {}
-        entity.["leg_nomedocliente@odata.bind"] = "/contacts(" + contactId + ")";
-        entity.leg_datadanotificacao = new Date();
-        entity.leg_mensagemdanotificacao = `Bem vindo a UniCode, ${contactName}!\nAgradecemos a sua escolha, sua matrícula foi concluída, agora é com você. Dê o seu melhor e se destaque!\nAtt. Equipe UniCode`;
-
-        Xrm.WebApi.online.createRecord("leg_notificacaoaocliente", entity).then(
-            function success(result) {
-                var newEntityId = result.id;
-
-                Treinamento.Account.DynamicsCustomAlert("Uma nova conta foi criada com o ID " + newEntityId, "ContaCriada");
-            },
-            function (error) {
-                Treinamento.Account.DynamicsCustomAlert(error.message, "Error");
-            }
-        );
+        leg_oportunidadeid: 'leg_oportunidadeid'        
     },
     OnSave: function (context) {
-        OnSaveStatusCode(context);
+        debugger;
         var formContext = context.getFormContext();
 
         var attributeName = Unicode.Opportunity.Attributos.leg_oportunidadeid;
@@ -59,11 +16,11 @@ Unicode.Opportunity = {
         Xrm.WebApi.online.retrieveMultipleRecords("opportunity", "?$select=leg_oportunidadeid&$filter=leg_oportunidadeid eq '" + id + "'").then(
             function success(results) {
                 if (results.entities.length > 0) {
-                    id = !formContext.getAttribute(attributeName).getValue() ? Unicode.Opportunity.geraCodigo() : formContext.getAttribute(attributeName).getValue();                   
+                    id = !formContext.getAttribute(attributeName).getValue() ? Unicode.Opportunity.geraCodigo() : formContext.getAttribute(attributeName).getValue();
                     formContext.getAttribute(attributeName).setValue(id);
                 }
                 else {
-                    
+
                     id = !formContext.getAttribute(attributeName).getValue() ? id : formContext.getAttribute(attributeName).getValue();
                     formContext.getAttribute(attributeName).setValue(id);
 
@@ -72,7 +29,7 @@ Unicode.Opportunity = {
             function (error) {
                 Unicode.Opportunity.DynamicsCustomAlert(error.message, "Erro com a Query de Contatos!");
             }
-        );      
+        );
 
     },
     rand: function (min, max) {
